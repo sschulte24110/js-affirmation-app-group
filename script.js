e1fe89e698269ba5b595ba16ec110486b6c4be65
Scott Schulte <scott.r.schulte@gmail.com>
2024-05-03 13:27:21 -0500

    Complete Stretch Goals

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 onReady();
+let fireCounter = 0;
+let iceCounter = 0;
 
 function onReady() {
   console.log('Javascript is working!');
@@ -8,12 +10,18 @@ function fireButton(event) {
   // console.log('fire button');
   const fireEmoji = document.getElementById('emoji-div'); // Target where you want the new item to be inserted
   fireEmoji.innerHTML += `<div>🔥</div>`;
+
+  fireCounter++;
+  console.log(fireCounter);
 }
 
 function iceButton(event) {
   // console.log('ice button');
   const iceEmoji = document.getElementById('emoji-div');
   iceEmoji.innerHTML += `<div>❄️</div>`;
+
+  iceCounter++;
+  console.log(iceCounter);
 }
 
 function createAffirmation(event) {
@@ -21,20 +29,23 @@ function createAffirmation(event) {
   event.preventDefault();
   const affirmation = document.getElementById('affirmation-input');
   const author = document.getElementById('author-input');
-  console.log(`${affirmation.value}, ${author.value}`);
+  // console.log(`${affirmation.value}, ${author.value}`);
 
-  document.getElementById('table-body').innerHTML += `
+  if (fireCounter >= iceCounter) {
+    document.getElementById('table-body').innerHTML += `
   <tr>
     <td>${affirmation.value}</td>
     <td>${author.value}</td>
     <td><button class="button-color" onclick="deleteAffirmation(event)">&#x2718</button></td>
   </tr>
   `;
-
+  }
   affirmation.value = ``;
   author.value = ``;
 }
 
 function deleteAffirmation(event) {
-  event.target.parentElement.remove();
+  if (iceCounter >= fireCounter) {
+    event.target.closest('tr').remove();
+  }
 }
