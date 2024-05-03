7ad76c2ac0be760ac304f18afbcc1676a4ee15c0
Scott Schulte <scott.r.schulte@gmail.com>
2024-05-03 10:59:16 -0500

    Both buttons are adding emojis to the DOM

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ onReady();
 function onReady() {
   console.log('Javascript is working!');
 }
+
+function fireButton(event) {
+  // console.log('fire button');
+  const fireEmoji = document.getElementById('emoji-div'); // Target where you want the new item to be inserted
+  fireEmoji.innerHTML += `<div>🔥</div>`;
+}
+
+function iceButton(event) {
+  // console.log('ice button');
+  const iceEmoji = document.getElementById('emoji-div');
+  iceEmoji.innerHTML += `<div>❄️</div>`;
+}
